11229321339dd04c8515cff9d98e7644dd1a8301
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-11-08 19:42:45 +0700

    update class collection_text

---
+++ cart.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+require_once("./controller/connection.php");
+
+$stmt = $conn->prepare("SELECT * FROM users");
+$stmt->execute();
+$users = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['login'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $ada = false;
+
+        foreach($users as $key => $value){
+            if ($username == $value['username']){
+                if (md5($password) == $value['password']){
+                    $_SESSION['active'] = $value['id_user'];
+                    if ($value['roles'] == "admin"){
+                        header("Location: ./admin/index.php");
+                    }
+                    else{
+                        header("Location: ./index.php");
+                    }
+                }
+                else{
+                    echo "<script>alert('Wrong Password!')</script>";
+                    echo "<script>window.location = './login.php'</script>";
+                }
+                $ada = true;
+            }
+        }
+
+        if (!$ada){
+            echo "<script>alert('Username not found !')</script>";
+            echo "<script>window.location = './login.php'</script>";
+        }
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <title>Sign In</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <?php require_once("./section/connection_head.php") ?>
+</head>
+<body class="main-layout">
+    <div class="header_section">
+        <?php require_once("./section/nav_section.php") ?>
+    </div>
+    <div class="collection_text">Cart</div>
+    <div class="layout_padding contact_section">
+        <div class="container-fluid ram">
+            <div class="row">
+                <div class="col-md-12">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php require_once("./section/footer_section.php") ?>
+    <?php require_once("./section/script_section.php") ?>
+</body>
+
+</html>
--- shoes.php
+++ shoes.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once("./controller/connection.php");
-$stmt = $conn->prepare("SELECT * FROM sepatu");
+$stmt = $conn->prepare("SELECT * FROM sepatu ORDER BY 1 DESC");
 $stmt->execute();
 $sepatu = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 ?>
@@ -17,7 +17,7 @@ $sepatu = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 	<meta name="keywords" content="">
 	<meta name="description" content="">
 	<meta name="author" content="">
-	
+
 	<?php require_once("./section/connection_head.php") ?>
 </head>
 
@@ -28,39 +28,31 @@ $sepatu = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 	<div class="collection_text">Shoes</div>
 	<div class="layout_padding gallery_section">
 		<div class="container">
-			<?php
-			$indexDecrement = count($sepatu) - 1;
-
-			while ($indexDecrement > 0) {
-			?>
-				<div class="row">
-					<?php
-					for ($i = 0; $i < 3; $i++) {
-						$lokasi = './admin/' . $sepatu[$indexDecrement]['link_gambarsepatu'];
-					?>
-						<div class="col-sm-12 col-md-6 col-lg-4" style="margin: 15px 0;">
-							<div class="best_shoes flex-center flex-column flex-hend flex-between">
-								<div>
-									<p class="best_text"><?= $sepatu[$indexDecrement]['nama_sepatu'] ?></p>
-								</div>
-								<div>
-									<div class="shoes_icon"><img src='<?= $lokasi ?>'></div>
-									<div class="star_text flex-center flex-vend">
-										<div class="right_part">
-											<div class="shoes_price">Rp. <span style="color: #ff4e5b;"><?= $sepatu[$indexDecrement]['harga_sepatu'] ?></span></div>
-										</div>
+			<div class="row">
+				<?php
+				foreach ($sepatu as $key => $value) {
+					$lokasi = "./admin/" . $value['link_gambarsepatu'];
+				?>
+					<div class="col-sm-12 col-md-6 col-lg-4" style="margin: 15px 0;">
+						<div class="best_shoes flex-center flex-column flex-hend flex-between">
+							<div>
+								<p class="best_text"><?= $value['nama_sepatu'] ?></p>
+							</div>
+							<div>
+								<div class="shoes_icon"><img src='<?= $lokasi ?>'></div>
+								<div class="star_text flex-center flex-vend">
+									<button class="btn-success" style="border-radius: 4px;">Add to Chart</button>
+									<div class="right_part">
+										<div class="shoes_price">Rp. <span style="color: #ff4e5b;"><?= number_format($value['harga_sepatu'], 0, ',', '.') . ",-" ?></span></div>
 									</div>
 								</div>
 							</div>
 						</div>
-					<?php
-						$indexDecrement -= 1;
-					}
-					?>
-				</div>
-			<?php
-			}
-			?>
+					</div>
+				<?php
+				}
+				?>
+			</div>
 			<div class="buy_now_bt">
 				<button class="buy_text">Buy Now</button>
 			</div>
