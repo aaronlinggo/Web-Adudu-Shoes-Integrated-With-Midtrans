4112ef3a2bc6398bef4c0416f82c087b67ac029f
Samuel Gunawan <samgun9d32@gmail.com>
2021-11-13 18:58:17 +0700

    Housekeeping CSS #1
    Filter mana yang enggak kepake sama yang kepake, belum ada yang dihapus incase mau rollback

--- index.php
+++ index.php
@@ -27,11 +27,11 @@
 								<div class="carousel-inner">
 									<div class="carousel-item active">
 										<div class="row">
-											<div class="col-sm-1 padding_0">
+											<div class="col-sm-1 fullwidth">
 											</div>
 											<div class="col-sm-6">
 												<div class="banner_taital">
-													<h1 class="banner_text">New Shoes Model </h1>
+													<h1 class="banner_text">New Shoes Model</h1>
 													<h1 class="mens_text">
 														<?php
 															$stmt = $conn -> prepare("SELECT * FROM sepatu ORDER BY id_sepatu DESC LIMIT 1");
@@ -67,6 +67,52 @@
 		</div>
 		<div class="layout_padding collection_section">
 			<div class="container">
+				<h1 class="new_text">
+					<strong>New Collection</strong>
+				</h1>
+				<p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+				<div class="collection_section_2">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="about-img">
+								<button class="new_bt">New</button>
+								<div class="shoes-img">
+									<img src="./images/shoes-img1.png">
+								</div>
+								<p class="sport_text">Men Sports</p>
+								<div class="dolar_text">$ <strong style="color: #f12a47;">90</strong></div>
+								<div class="star_icon">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+							</div>
+							<button class="seemore_bt">See More</button>
+						</div>
+						<div class="col-md-6">
+							<div class="about-img2">
+								<div class="shoes-img2">
+									<img src="./images/shoes-img2.png">
+								</div>
+								<p class="sport_text">Men Sports</p>
+								<div class="dolar_text">$ <strong style="color: #f12a47;">90</strong></div>
+								<div class="star_icon">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div class="collection_section">
@@ -107,14 +153,153 @@
 		</div>
 		<div class="collection_section layout_padding">
 			<div class="container">
-				<strong>Empty Section #1</strong>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque quas saepe qui labore, molestias suscipit sed aliquam nam reiciendis velit fuga explicabo pariatur ducimus iste quam, odit quasi quo nostrum, inventore culpa sunt voluptate deleniti omnis tempore. Fugiat repudiandae sapiente temporibus est, natus magni alias iste totam quisquam corporis nihil, enim, iusto officiis ipsum voluptatum nobis accusantium a quos assumenda non. Alias hic dolores eius cupiditate voluptatem culpa amet inventore aliquam neque molestias id fugiat tempore debitis quam magnam, asperiores, doloremque deleniti ipsum assumenda maiores quo laboriosam optio! Cupiditate vel harum facilis sed nesciunt quas necessitatibus eaque veniam molestiae reprehenderit!
+				<h1 class="new_text">
+					<strong>New Arrivals Products</strong>
+				</h1>
+				<p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
 			</div>
 		</div>
 		<div class="layout_padding gallery_section">
 			<div class="container">
-				<strong>Empty Section #2</strong>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque quas saepe qui labore, molestias suscipit sed aliquam nam reiciendis velit fuga explicabo pariatur ducimus iste quam, odit quasi quo nostrum, inventore culpa sunt voluptate deleniti omnis tempore. Fugiat repudiandae sapiente temporibus est, natus magni alias iste totam quisquam corporis nihil, enim, iusto officiis ipsum voluptatum nobis accusantium a quos assumenda non. Alias hic dolores eius cupiditate voluptatem culpa amet inventore aliquam neque molestias id fugiat tempore debitis quam magnam, asperiores, doloremque deleniti ipsum assumenda maiores quo laboriosam optio! Cupiditate vel harum facilis sed nesciunt quas necessitatibus eaque veniam molestiae reprehenderit!
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="best_shoes">
+							<p class="best_text">Best Shoes </p>
+							<div class="shoes_icon">
+								<img src="./images/shoes-img4.png">
+							</div>
+							<div class="star_text">
+								<div class="left_part">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+								<div class="right_part">
+									<div class="shoes_price">$ <span style="color: #ff4e5b;">60</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="best_shoes">
+							<p class="best_text">Best Shoes </p>
+							<div class="shoes_icon">
+								<img src="./images/shoes-img5.png">
+							</div>
+							<div class="star_text">
+								<div class="left_part">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+								<div class="right_part">
+									<div class="shoes_price">$ <span style="color: #ff4e5b;">400</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="best_shoes">
+							<p class="best_text">Best Shoes </p>
+							<div class="shoes_icon">
+								<img src="./images/shoes-img6.png">
+							</div>
+							<div class="star_text">
+								<div class="left_part">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+								<div class="right_part">
+									<div class="shoes_price">$ <span style="color: #ff4e5b;">50</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-4">
+						<div class="best_shoes">
+							<p class="best_text">Sports Shoes</p>
+							<div class="shoes_icon">
+								<img src="./images/shoes-img7.png">
+							</div>
+							<div class="star_text">
+								<div class="left_part">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+								<div class="right_part">
+									<div class="shoes_price">$ <span style="color: #ff4e5b;">70</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="best_shoes">
+							<p class="best_text">Sports Shoes</p>
+							<div class="shoes_icon">
+								<img src="./images/shoes-img8.png">
+							</div>
+							<div class="star_text">
+								<div class="left_part">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+								<div class="right_part">
+									<div class="shoes_price">$ <span style="color: #ff4e5b;">100</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="best_shoes">
+							<p class="best_text">Sports Shoes</p>
+							<div class="shoes_icon">
+								<img src="./images/shoes-img9.png">
+							</div>
+							<div class="star_text">
+								<div class="left_part">
+									<ul>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+										<li><a href="#"><img src="./images/star-icon.png"></a></li>
+									</ul>
+								</div>
+								<div class="right_part">
+									<div class="shoes_price">$ <span style="color: #ff4e5b;">90</span></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="buy_now_bt">
+					<button class="buy_text">Buy Now</button>
+				</div>
 			</div>
 		</div>
 		<?php require_once("./section/footer_section.php") ?>
