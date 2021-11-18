c4d84629d06ab0f3fc4b36c1d22dafddc2ec5ab5
Samuel Gunawan <samgun9d32@gmail.com>
2021-11-18 22:00:29 +0700

    Update transaction.php

--- midtrans/application/views/transaction.php
+++ midtrans/application/views/transaction.php
@@ -191,7 +191,7 @@
 		<script src="../../js/plugin.js"></script>
 		<script src="../../js/jquery.mCustomScrollbar.concat.min.js"></script>
 		<script src="../../js/custom.js"></script> -->
-		<script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 		<script>
 			$(document).ready(function() {
 				$(".fancybox").fancybox({
