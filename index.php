e484797db0934e5b942def43bf051dca47215440
Samuel Gunawan <samgun9d32@gmail.com>
2021-11-18 13:13:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 		</div>
 		<div class="layout_padding collection_section">
 			<div class="container">
-				<h1 class="new_text">
+				<!-- <h1 class="new_text">
 					<strong>New Collection</strong>
 				</h1>
 				<p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
@@ -100,7 +100,7 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		</div>
 		<div class="collection_section">
@@ -136,157 +136,6 @@
 						<?php
 						?>
 					</div>
-				</div>
-			</div>
-		</div>
-		<div class="collection_section layout_padding">
-			<div class="container">
-				<h1 class="new_text">
-					<strong>New Arrivals Products</strong>
-				</h1>
-				<p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-			</div>
-		</div>
-		<div class="layout_padding gallery_section">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-4">
-						<div class="best_shoes">
-							<p class="best_text">Best Shoes </p>
-							<div class="shoes_icon">
-								<img src="./images/shoes-img4.png">
-							</div>
-							<div class="star_text">
-								<div class="left_part">
-									<ul>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-									</ul>
-								</div>
-								<div class="right_part">
-									<div class="shoes_price">$ <span style="color: #ff4e5b;">60</span></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="best_shoes">
-							<p class="best_text">Best Shoes </p>
-							<div class="shoes_icon">
-								<img src="./images/shoes-img5.png">
-							</div>
-							<div class="star_text">
-								<div class="left_part">
-									<ul>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-									</ul>
-								</div>
-								<div class="right_part">
-									<div class="shoes_price">$ <span style="color: #ff4e5b;">400</span></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="best_shoes">
-							<p class="best_text">Best Shoes </p>
-							<div class="shoes_icon">
-								<img src="./images/shoes-img6.png">
-							</div>
-							<div class="star_text">
-								<div class="left_part">
-									<ul>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-									</ul>
-								</div>
-								<div class="right_part">
-									<div class="shoes_price">$ <span style="color: #ff4e5b;">50</span></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-4">
-						<div class="best_shoes">
-							<p class="best_text">Sports Shoes</p>
-							<div class="shoes_icon">
-								<img src="./images/shoes-img7.png">
-							</div>
-							<div class="star_text">
-								<div class="left_part">
-									<ul>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-									</ul>
-								</div>
-								<div class="right_part">
-									<div class="shoes_price">$ <span style="color: #ff4e5b;">70</span></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="best_shoes">
-							<p class="best_text">Sports Shoes</p>
-							<div class="shoes_icon">
-								<img src="./images/shoes-img8.png">
-							</div>
-							<div class="star_text">
-								<div class="left_part">
-									<ul>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-									</ul>
-								</div>
-								<div class="right_part">
-									<div class="shoes_price">$ <span style="color: #ff4e5b;">100</span></div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-sm-4">
-						<div class="best_shoes">
-							<p class="best_text">Sports Shoes</p>
-							<div class="shoes_icon">
-								<img src="./images/shoes-img9.png">
-							</div>
-							<div class="star_text">
-								<div class="left_part">
-									<ul>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-										<li><a href="#"><img src="./images/star-icon.png"></a></li>
-									</ul>
-								</div>
-								<div class="right_part">
-									<div class="shoes_price">$ <span style="color: #ff4e5b;">90</span></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="buy_now_bt">
-					<button class="buy_text">Buy Now</button>
 				</div>
 			</div>
 		</div>
