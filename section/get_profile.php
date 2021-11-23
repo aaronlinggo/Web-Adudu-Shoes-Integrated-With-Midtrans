e6b1391570ae3736dc603f824160039dc50e77b3
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-11-23 19:24:34 +0700

    nambahin style profile :v

--- section/get_profile.php
+++ section/get_profile.php
@@ -5,8 +5,21 @@
 ?>
 
 <div>
-    <div>Nama: <?= $_REQUEST['data']['nama'] ?></div>
-    <div>Username: <?= $_REQUEST['data']['username'] ?></div>
-    <div>Email: <?= $_REQUEST['data']['email'] ?></div>
-    <div>Tanggal Lahir: <?= $_REQUEST['data']['tanggal_lahir'] ?></div>
+    <div class="text-center lorem_text bg-light card" style="background-color: #c4c4c7 !important;">User ID: <?= $_REQUEST['data']['id_user'] ?></div>
+    <div>
+        <div class="lorem_text font-weight-bold">Nama</div>
+        <?= $_REQUEST['data']['nama'] ?>
+    </div>
+    <div>
+        <div class="lorem_text font-weight-bold">Username</div>
+        <?= $_REQUEST['data']['username'] ?>
+    </div>
+    <div>
+        <div class="lorem_text font-weight-bold">Email</div>
+        <?= $_REQUEST['data']['email'] ?>
+    </div>
+    <div>
+        <div class="lorem_text font-weight-bold">Tanggal Lahir</div>
+        <?= $_REQUEST['data']['tanggal_lahir'] ?>
+    </div>
 </div>
