429687b4dcdfdf5a75fe33f07371bf531d7a50c0
Aaron Linggo Satria <aaronlinggosatria@gmail.com>
2021-11-24 12:27:41 +0700

    update profile

--- controller/connection.php
+++ controller/connection.php
@@ -1,10 +1,11 @@
 <?php
     $host = 'localhost';
-    $user = 'root';
-    $password = '';
-    $database = 'db_adudu';
+    $user = 'adudumas_admin';
+    $password = 'proyekkok2minggu';
+    $database = 'adudumas_db_adudu';
     $port = '3306';
-    $conn = new mysqli($host, $user, $password, $database);
+    // $conn = new mysqli($host, $user, $password, $database);
+    $conn = new mysqli($host, 'root', '', 'db_adudu');
 
     if($conn -> connect_errno) {
         die("Gagal Connect: " . $conn -> connect_error);
